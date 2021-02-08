4f0108b032f69fab975da1616a256adfe9ffb3a6
liucz25 <liuchangzheng25@163.com>
2021-02-08 23:12:38 +0800

    Add files via upload

---
+++ TTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <TTableDetail v-model="data"></TTableDetail>
+</template>
+
+<script>
+import TTableDetail from './TTableDetail'
+
+export default {
+  components: { TTableDetail },
+  data() {
+    return {
+      data:{
+   
+        tableInfr:{TableName:'table',
+                    tableMethd:'post'},
+
+        
+       tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }],
+        }
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ TTableDetail.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+
+    <table>
+        <thead>
+            <th v-for='(item,i) in value.tableData[0]'>{{i}}</th>
+        </thead>
+        <tbody>
+            <tr v-for='(item,i) in value.tableData' >
+                <td v-for='(i2,i3) in item'>{{i2}}</td>
+            </tr>
+        </tbody>
+    </table>  
+</div>   
+</template>
+<script>
+export default {
+    props:['value'],
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+        },
+    
+}
+</script>
+
+<style>
+
+</style>
